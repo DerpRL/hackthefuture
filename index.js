0e42e9ec34cd052e1c12897b02731f5411ecdfd8
yarne.savaete <yarne.savaete@student.howest.be>
2021-12-02 10:16:22 +0100

    Getting all the suspects

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 "use strict";
+
+const nodeFetch = require('node-fetch');
+
+
+const requestOptions = {
+    method: 'GET',
+    redirect: 'follow',
+    headers: {
+        'Content-Type': 'application/json',
+    }
+};
+
+function getAllSuspects(cb=null) {
+    nodeFetch('https://htf-2021.zinderlabs.com/suspect', requestOptions).then(response => {
+        response.json().then(jsonResponse => {
+            console.log(jsonResponse)
+        }).catch(err => console.error(err))
+    }).catch(err => console.error(err))
+}
+
+getAllSuspects()
